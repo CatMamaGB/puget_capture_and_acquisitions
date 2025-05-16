41b49b775eaa8ac08d4253c2d170a7a7d0282daf
CatMamaGB <briannapirro1990@gmail.com>
2025-05-15 20:59:24 -0500

    google tag update

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,9 @@
-import Head from 'next/head'
+import { GoogleAnalytics } from '@/components/GoogleAnalytics'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        {/* Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=G-FSFBG0LJPH`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-FSFBG0LJPH', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-      </Head>
+      <GoogleAnalytics />
       <Component {...pageProps} />
     </>
   )
