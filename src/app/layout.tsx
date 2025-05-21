58b141b9c2a005b57c66e38e0b68945454a1b954
CatMamaGB <briannapirro1990@gmail.com>
2025-05-21 13:39:36 -0500

    favicon.ico changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,9 +54,11 @@ export const metadata: Metadata = {
       { url: '/favicon-144x144.png', sizes: '144x144', type: 'image/png' },
       { url: '/favicon-192x192.png', sizes: '192x192', type: 'image/png' },
       { url: '/favicon-512x512.png', sizes: '512x512', type: 'image/png' },
-      { url: '/favicon.svg', type: 'image/svg+xml' }
+      { url: '/favicon.svg', type: 'image/svg+xml', sizes: 'any' }
     ],
-    apple: '/apple-touch-icon.png',
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+    ],
   },
 };
 
